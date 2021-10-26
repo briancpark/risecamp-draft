5159dec9672aac8c31b47c0e89a9a531805263cd
Brian Park <briancpark@berkeley.edu>
2021-10-26 00:24:45 +0000

    add script to download the dataset

---
+++ download_data.py
@@ -0,0 +1,4 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+for year in range(1804, 2022):
+    gdd.download_file_from_google_drive(file_id='1gHdXtzZKpiaUdSyMXyP-9q7x_GQF6Umi', dest_path='./' + year + '.csv')
